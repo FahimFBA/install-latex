a21bd35f6108190f184f39c6bb2ce240e4e6fa7d
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2025-02-19 20:34:35 +0600

    Update GitHub Pages deployment workflow and modify Docusaurus site title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Install LaTeX Locally',
+  tagline: 'Install LaTeX Locally on Windows, MacOS, and Linux',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
